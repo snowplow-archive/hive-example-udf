2e9ef5582669141479ddaa9becef9fb873ea122c
Alex Dean <alexander.dean@keplarllp.com>
2013-01-27 21:47:01 +0000

    Added in files

---
+++ build.sbt
@@ -0,0 +1,23 @@
+import AssemblyKeys._
+
+name := "sample-hive-udf"
+
+version := "0.0.1"
+
+organization := "com.snowplowanalytics"
+
+scalaVersion := "2.9.2"
+
+scalacOptions ++= Seq("-unchecked", "-deprecation")
+
+resolvers += "CDH4" at "https://repository.cloudera.com/artifactory/cloudera-repos/"
+
+libraryDependencies += "org.apache.hadoop" %  "hadoop-core"          % "0.20.2"     % "provided"
+
+libraryDependencies += "org.apache.hive" %  "hive-exec"          % "0.8.1"       % "provided"
+
+libraryDependencies += "org.specs2"      %% "specs2"               % "1.12.1"      % "test"
+
+parallelExecution in Test := false
+
+seq(assemblySettings: _*)
---
+++ src/main/java/com/snowplowanalytics/hive/udf/ToUpper.java
@@ -0,0 +1,27 @@
+package com.snowplowanalytics.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.io.Text;
+
+@Description(
+	     name = "toupper",
+	     value = "_FUNC_(str) - Converts a string to uppercase",
+	     extended = "Example:\n" +
+	     "  > SELECT toupper(author_name) FROM authors a;\n" +
+	     "  STEPHEN KING"
+	     )
+public class ToUpper extends UDF {
+
+    public Text evaluate(Text s) {
+		Text to_value = null;
+		if (s != null) {
+		    try { 
+				to_value.set(s.toString().toUpperCase());
+		    } catch (Exception e) { // Should never happen
+				to_value = new Text(s);
+		    }
+		}
+		return to_value;
+    }
+}
